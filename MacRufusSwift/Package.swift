2dbe1660eeec8da4deee01a277c02b2de7d76d3b
Brian Gan <briangangster@gmail.com>
2026-08-12 20:21:36 -0400

    Package: added Subprocess package

--- MacRufusSwift/Package.swift
+++ MacRufusSwift/Package.swift
@@ -4,9 +4,18 @@ import PackageDescription
 let package = Package(
     name: "MacRufusSwift",
     platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(
+            url: "https://github.com/swiftlang/swift-subprocess.git",
+            branch: "main"
+        )
+    ],
     targets: [
         .executableTarget(
             name: "MacRufusSwift",
+            dependencies: [
+                .product(name: "Subprocess", package: "swift-subprocess")
+            ],
             path: "Sources/MacRufusSwift"
         )
     ]
